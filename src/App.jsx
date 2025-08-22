b72f976f725348eb30e36257f4a05c0d1fe70adb
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2025-08-23 00:32:54 +1200

    made nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
 import Profile from "./components/Profile/Profile";
+import Home from "./components/Home/Home";
 
 import "./App.css";
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/home-page" element={<Home />} />
         <Route path="/profile-page" element={<Profile />} />
       </Routes>
     </div>
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DisplayPage from "../DisplayPage/DisplayPage";
+import Nav from "../Nav/Nav";
+
+import "../../styles/homestyles.css";
+const Home = () => {
+  return (
+    <div className="-main-container__wrapper">
+      <Nav />
+    </div>
+  );
+};
+
+export default Home;
