bd41de68ec141313e699884caca015742ac58c9e
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2025-08-27 22:23:13 +1200

    added some content in the feed page

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import Nav from "../Nav/Nav";
 
+import "../../styles/feedstyles.css";
 const Feed = () => {
   return (
     <div className="-main-container__wrapper">
       <Nav />
+      <div className="feed-container__wrapper">
+        <div className="-display-flex-justified-center">
+          <div className="feed-asksomething__wrapper">
+            <img
+              className="feed-picture-thumbnail__img"
+              src="https://randomuser.me/api/portraits/men/1.jpg"
+              alt="profile-picture-thumbnail"
+            />
+            <textarea
+              className="feed-asksomething-message__textarea"
+              placeholder="Ask something..."
+            ></textarea>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
