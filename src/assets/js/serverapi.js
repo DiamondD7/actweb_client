b623075b9526584a5e1b459757b6de1d579cec0b
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2025-12-11 20:11:36 +1300

    added dockerfile

--- src/assets/js/serverapi.js
+++ src/assets/js/serverapi.js
@@ -1,74 +1,69 @@
+const userapiVar = import.meta.env.VITE_USER_API;
+const postapiVar = import.meta.env.VITE_POST_API;
+const chatapiVar = import.meta.env.VITE_CHAT_API;
+const notificationapiVar = import.meta.env.VITE_NOTIFICATION_API;
+
 //UserAPI
-export const BASE_URL = "http://localhost:5016";
-export const USER_API_URI = "http://localhost:5016/api/Users";
-export const ValidateToken =
-  "http://localhost:5016/api/Users/validate-user-token";
-export const GetUserBackgrounds =
-  "http://localhost:5016/api/Users/get-user-backgrounds";
-export const GetUsersByIds = "http://localhost:5016/api/Users/userIds";
-export const GetFeaturedBackgrounds =
-  "http://localhost:5016/api/Users/get-featured-backgrounds";
-export const UserCreation = "http://localhost:5016/api/Users/create-user";
-export const CheckLogin = "http://localhost:5016/api/Users/check-login";
-export const ValidateTokens = "http://localhost:5016/api/Users/validate-tokens";
-export const UploadProfilePicture =
-  "http://localhost:5016/api/Users/upload-profile-picture";
-export const UpdateData = "http://localhost:5016/api/Users/update-user-data";
-export const UpdateUserBackground =
-  "http://localhost:5016/api/Users/update-user-background";
-export const AddUserBackground =
-  "http://localhost:5016/api/Users/add-user-background";
-export const Logout = "http://localhost:5016/api/Users/logout";
+export const BASE_URL = userapiVar;
+export const USER_API_URI = `${userapiVar}/api/Users`;
+export const ValidateToken = `${userapiVar}/api/Users/validate-user-token`;
+export const GetUserBackgrounds = `${userapiVar}/api/Users/get-user-backgrounds`;
+export const GetUsersByIds = `${userapiVar}/api/Users/userIds`;
+export const GetFeaturedBackgrounds = `${userapiVar}/api/Users/get-featured-backgrounds`;
+export const UserCreation = `${userapiVar}/api/Users/create-user`;
+export const CheckLogin = `${userapiVar}/api/Users/check-login`;
+export const ValidateTokens = `${userapiVar}/api/Users/validate-tokens`;
+export const UploadProfilePicture = `${userapiVar}/api/Users/upload-profile-picture`;
+export const UpdateData = `${userapiVar}/api/Users/update-user-data`;
+export const UpdateUserBackground = `${userapiVar}/api/Users/update-user-background`;
+export const AddUserBackground = `${userapiVar}/api/Users/add-user-background`;
+export const Logout = `${userapiVar}/api/Users/logout`;
 
 //UserAPI / SavedPosts
-export const GetSavedPostIds = "http://localhost:5016/api/SavedPost/saved-ids";
-export const SavePost = "http://localhost:5016/api/SavedPost/saved-post";
+export const GetSavedPostIds = `${userapiVar}/api/SavedPost/saved-ids`;
+export const SavePost = `${userapiVar}/api/SavedPost/saved-post`;
 
 //-----FOLLOWING
-export const AddFollowing = "http://localhost:5016/api/Following";
-export const DiscoverUsers = "http://localhost:5016/api/Following/users";
-export const GetFollowing = "http://localhost:5016/api/Following/following";
-export const GetFollowers = "http://localhost:5016/api/Following/followers";
-export const UnFollow = "http://localhost:5016/api/Following";
+export const AddFollowing = `${userapiVar}/api/Following`;
+export const DiscoverUsers = `${userapiVar}/api/Following/users`;
+export const GetFollowing = `${userapiVar}/api/Following/following`;
+export const GetFollowers = `${userapiVar}/api/Following/followers`;
+export const UnFollow = `${userapiVar}/api/Following`;
 
 //-----PostAPI
-export const BASE_POST_API = "http://localhost:5123";
-export const UpdatePost = "http://localhost:5123/api/Post";
-export const GetPosts = "http://localhost:5123/api/Post/get-posts";
-export const GetReels = "http://localhost:5123/api/Post/reels";
-export const GetFollowingPosts =
-  "http://localhost:5123/api/Post/postsoffollowing";
-export const UploadVideo = "http://localhost:5123/api/Post/upload-video";
-export const AddPost = "http://localhost:5123/api/Post/add-post";
-export const GetPostsByIds = "http://localhost:5123/api/Post/posts-by-ids";
+export const BASE_POST_API = postapiVar;
+export const UpdatePost = `${postapiVar}/api/Post`;
+export const GetPosts = `${postapiVar}/api/Post/get-posts`;
+export const GetReels = `${postapiVar}/api/Post/reels`;
+export const GetFollowingPosts = `${postapiVar}/api/Post/postsoffollowing`;
+export const UploadVideo = `${postapiVar}/api/Post/upload-video`;
+export const AddPost = `${postapiVar}/api/Post/add-post`;
+export const GetPostsByIds = `${postapiVar}/api/Post/posts-by-ids`;
 
 //POST COMMENT
-export const AddComment = "http://localhost:5123/api/Comment";
-export const GetComments = "http://localhost:5123/api/Comment/comments";
+export const AddComment = `${postapiVar}/api/Comment`;
+export const GetComments = `${postapiVar}/api/Comment/comments`;
 
 //POST LIKE
-export const AddLike = "http://localhost:5123/api/Like";
-export const GetLikes = "http://localhost:5123/api/Like/likes";
-export const CheckLike = "http://localhost:5123/api/Like/user-like";
+export const AddLike = `${postapiVar}/api/Like`;
+export const GetLikes = `${postapiVar}/api/Like/likes`;
+export const CheckLike = `${postapiVar}/api/Like/user-like`;
 
 //ChatAPI
-export const GetMessages = "http://localhost:5188/api/Message/chatroom";
-export const GetPreviewMessages =
-  "http://localhost:5188/api/Message/message-preview";
-export const GetChats = "http://localhost:5188/api/Message/chats";
-export const CreateChatRoom = "http://localhost:5188/api/Message/chatroom";
-export const MessageSent = "http://localhost:5188/api/Message/message";
-export const MarkSeenMessage = "http://localhost:5188/api/Message";
+export const GetMessages = `${chatapiVar}/api/Message/chatroom`;
+export const GetPreviewMessages = `${chatapiVar}/api/Message/message-preview`;
+export const GetChats = `${chatapiVar}/api/Message/chats`;
+export const CreateChatRoom = `${chatapiVar}/api/Message/chatroom`;
+export const MessageSent = `${chatapiVar}/api/Message/message`;
+export const MarkSeenMessage = `${chatapiVar}/api/Message`;
 
 //NotificationAPI
-export const GetNotificationIds =
-  "http://localhost:5164/api/Notification/notification/ids";
-export const SaveNotification =
-  "http://localhost:5164/api/Notification/notification";
+export const GetNotificationIds = `${notificationapiVar}/api/Notification/notification/ids`;
+export const SaveNotification = `${notificationapiVar}/api/Notification/notification`;
 
 //CastingCallAPI
 export const GetCastingCalls =
   "http://localhost:5067/api/CastingCall/casting-calls";
 
 //EventsAPI
-export const GetAllEvents = "http://localhost:5041/api/Events";
+export const GetAllEvents = `http://localhost:5041/api/Events`;
