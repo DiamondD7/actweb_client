b7e0c706fdf3a387e1b4fb9e76559e0f7f278a8e
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2025-12-15 16:34:13 +1300

    added btn in security advance

--- src/components/Settings/Sub-Settings/AdvanceSecurity.jsx
+++ src/components/Settings/Sub-Settings/AdvanceSecurity.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { LockSimpleOpenIcon } from "@phosphor-icons/react";
 
 const AdvanceSecurity = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <div className="-display-flex-aligned-center -gap-20">
+        <h5>Two Factor Authentication</h5>
+        <button className="advance-security-radio-disabled__btn">
+          Disabled <LockSimpleOpenIcon size={20} color="gray" />
+        </button>
+      </div>
     </div>
   );
 };
